0aa2c6263ac18281d3766c42a325d5febfac366f
SLUZ56 <sd54diasinacio@gmail.com>
2024-01-24 21:28:47 -0300

    file modified

--- src/app.js
+++ src/app.js
@@ -7,10 +7,9 @@ import cors from 'cors';
 class App {
   constructor() {
     this.app = express();
-
+    this.app.use(cors());
     this.middlewares();
     this.routes();
-    this.app.use(cors());
   }
 
   middlewares() {
